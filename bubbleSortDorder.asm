2e05e415b0146a444ff869f09253c9984740951e
Shubham Varshney <varshneyshubham98@gmail.com>
2017-02-02 22:02:37 +0530

    Create bubbleSortDorder.asm

---
+++ bubbleSortDorder.asm
@@ -0,0 +1,21 @@
+MVI D,06H
+loop: LXI H,0000H
+MVI C,06H
+again: MOV A,M
+nextNumber: INX H
+DCR C
+JZ mainDecrement
+CMP M
+JC swap
+MOV A,M
+JMP nextNumber
+swap: MOV B,M
+MOV M,A
+DCX H
+MOV M,B
+INX H
+JMP again
+mainDecrement: DCR D
+JZ end
+JMP loop
+end: HLT
